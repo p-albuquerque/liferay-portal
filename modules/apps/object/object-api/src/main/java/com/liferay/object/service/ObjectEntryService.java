7331d0c164803c2b2256d7b3bbbdeab8cd37dd75
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2023-01-10 16:45:07 -0300

    Revert "LPS-169697 buildService"
    This reverts commit d47d9185008e2433a96fce21b041adf3e42c6d05.

--- modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryService.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectEntryService.java
@@ -18,7 +18,6 @@ import com.liferay.object.model.ObjectEntry;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.access.control.AccessControlled;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.service.BaseService;
@@ -131,11 +130,6 @@ public interface ObjectEntryService extends BaseService {
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public boolean hasModelResourcePermission(
 			ObjectEntry objectEntry, String actionId)
-		throws PortalException;
-
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public boolean hasModelResourcePermission(
-			User user, long objectEntryId, String actionId)
 		throws PortalException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/http/ObjectEntryServiceHttp.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/http/ObjectEntryServiceHttp.java
@@ -639,48 +639,6 @@ public class ObjectEntryServiceHttp {
 		}
 	}
 
-	public static boolean hasModelResourcePermission(
-			HttpPrincipal httpPrincipal,
-			com.liferay.portal.kernel.model.User user, long objectEntryId,
-			String actionId)
-		throws com.liferay.portal.kernel.exception.PortalException {
-
-		try {
-			MethodKey methodKey = new MethodKey(
-				ObjectEntryServiceUtil.class, "hasModelResourcePermission",
-				_hasModelResourcePermissionParameterTypes14);
-
-			MethodHandler methodHandler = new MethodHandler(
-				methodKey, user, objectEntryId, actionId);
-
-			Object returnObj = null;
-
-			try {
-				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
-			}
-			catch (Exception exception) {
-				if (exception instanceof
-						com.liferay.portal.kernel.exception.PortalException) {
-
-					throw (com.liferay.portal.kernel.exception.PortalException)
-						exception;
-				}
-
-				throw new com.liferay.portal.kernel.exception.SystemException(
-					exception);
-			}
-
-			return ((Boolean)returnObj).booleanValue();
-		}
-		catch (com.liferay.portal.kernel.exception.SystemException
-					systemException) {
-
-			_log.error(systemException, systemException);
-
-			throw systemException;
-		}
-	}
-
 	public static boolean hasPortletResourcePermission(
 			HttpPrincipal httpPrincipal, long groupId, long objectDefinitionId,
 			String actionId)
@@ -689,7 +647,7 @@ public class ObjectEntryServiceHttp {
 		try {
 			MethodKey methodKey = new MethodKey(
 				ObjectEntryServiceUtil.class, "hasPortletResourcePermission",
-				_hasPortletResourcePermissionParameterTypes15);
+				_hasPortletResourcePermissionParameterTypes14);
 
 			MethodHandler methodHandler = new MethodHandler(
 				methodKey, groupId, objectDefinitionId, actionId);
@@ -731,7 +689,7 @@ public class ObjectEntryServiceHttp {
 		try {
 			MethodKey methodKey = new MethodKey(
 				ObjectEntryServiceUtil.class, "updateObjectEntry",
-				_updateObjectEntryParameterTypes16);
+				_updateObjectEntryParameterTypes15);
 
 			MethodHandler methodHandler = new MethodHandler(
 				methodKey, objectEntryId, values, serviceContext);
@@ -816,14 +774,10 @@ public class ObjectEntryServiceHttp {
 			com.liferay.object.model.ObjectEntry.class, String.class
 		};
 	private static final Class<?>[]
-		_hasModelResourcePermissionParameterTypes14 = new Class[] {
-			com.liferay.portal.kernel.model.User.class, long.class, String.class
-		};
-	private static final Class<?>[]
-		_hasPortletResourcePermissionParameterTypes15 = new Class[] {
+		_hasPortletResourcePermissionParameterTypes14 = new Class[] {
 			long.class, long.class, String.class
 		};
-	private static final Class<?>[] _updateObjectEntryParameterTypes16 =
+	private static final Class<?>[] _updateObjectEntryParameterTypes15 =
 		new Class[] {
 			long.class, java.util.Map.class,
 			com.liferay.portal.kernel.service.ServiceContext.class
