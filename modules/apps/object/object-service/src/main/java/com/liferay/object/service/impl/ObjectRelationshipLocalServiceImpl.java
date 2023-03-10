4e5a5a24751744d2d7133709c2981daadacb3689
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2023-03-10 17:27:14 -0300

    LPS-111111 Create method to get All ObjectRelationships by child ObjectDefinition

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
@@ -570,6 +570,14 @@ public class ObjectRelationshipLocalServiceImpl
 			objectDefinitionId1, deletionType, reverse);
 	}
 
+	@Override
+	public List<ObjectRelationship> getObjectRelationshipsByObjectDefinitionId2(
+		long objectDefinitionId2) {
+
+		return objectRelationshipPersistence.findByObjectDefinitionId2(
+			objectDefinitionId2);
+	}
+
 	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public ObjectRelationship updateObjectRelationship(
