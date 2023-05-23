0efb9479f444debe39d47de6927c7d31e209728a
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2023-05-23 15:21:22 -0300

    LPS-184446 Create Exception Mapper

---
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/jaxrs/exception/mapper/ObjectFieldReadOnlyExceptionMapper.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.object.exception.ObjectFieldReadOnlyException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+/**
+ * @author Paulo Albuquerque
+ */
+public class ObjectFieldReadOnlyExceptionMapper
+	extends BaseExceptionMapper<ObjectFieldReadOnlyException> {
+
+	@Override
+	protected Problem getProblem(
+		ObjectFieldReadOnlyException objectFieldReadOnlyException) {
+
+		return new Problem(objectFieldReadOnlyException);
+	}
+
+}
