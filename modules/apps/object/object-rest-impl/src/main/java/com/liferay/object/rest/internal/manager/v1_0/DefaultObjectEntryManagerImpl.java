2340362e19a1a4c8a7d8d52dd035bfca827ab4fe
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2023-05-31 11:48:46 -0300

    LPS-184446 Apply ObjectEntryReadOnlyUtil validate

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -142,6 +142,10 @@ public class DefaultObjectEntryManagerImpl
 			String scopeKey)
 		throws Exception {
 
+		validateReadOnly(
+			0, dtoConverterContext, null, objectDefinition, objectEntry,
+			scopeKey);
+
 		com.liferay.object.model.ObjectEntry serviceBuilderObjectEntry =
 			_objectEntryService.addObjectEntry(
 				getGroupId(objectDefinition, scopeKey),
@@ -702,6 +706,11 @@ public class DefaultObjectEntryManagerImpl
 		_checkObjectEntryObjectDefinitionId(
 			objectDefinition, serviceBuilderObjectEntry);
 
+		validateReadOnly(
+			0, dtoConverterContext,
+			serviceBuilderObjectEntry.getExternalReferenceCode(),
+			objectDefinition, objectEntry, null);
+
 		serviceBuilderObjectEntry = _objectEntryService.updateObjectEntry(
 			objectEntryId,
 			_toObjectValues(
@@ -727,6 +736,10 @@ public class DefaultObjectEntryManagerImpl
 			String externalReferenceCode, ObjectDefinition objectDefinition,
 			ObjectEntry objectEntry, String scopeKey)
 		throws Exception {
+
+		validateReadOnly(
+			companyId, dtoConverterContext, externalReferenceCode,
+			objectDefinition, objectEntry, scopeKey);
 
 		long groupId = getGroupId(objectDefinition, scopeKey);
 
