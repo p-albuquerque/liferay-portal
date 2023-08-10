102700cab269b1bf838d513fc42107dd44cbb8a9
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-08-10 15:25:36 -0300

    LPS-192185 If the rootObjectDefinitionId is 0 it means that an unbind operation is being applied

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -904,6 +904,12 @@ public class ObjectDefinitionLocalServiceImpl
 		ObjectDefinition objectDefinition =
 			objectDefinitionPersistence.findByPrimaryKey(objectDefinitionId);
 
+		if (rootObjectDefinitionId == 0) {
+			objectDefinition.setRootObjectDefinitionId(0);
+
+			return objectDefinitionPersistence.update(objectDefinition);
+		}
+
 		ObjectDefinition rootObjectDefinition =
 			objectDefinitionPersistence.findByPrimaryKey(
 				rootObjectDefinitionId);
