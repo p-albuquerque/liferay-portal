1c0ef31f9766a8838a9dbe485123e8aae76a5be1
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-08-25 12:39:26 -0300

    LPS-192184 Adapt TreeTestUtil to avoid duplications

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/util/TreeTestUtil.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/util/TreeTestUtil.java
@@ -5,7 +5,7 @@
 
 package com.liferay.object.service.test.util;
 
-import com.liferay.object.constants.ObjectRelationshipConstants;
+import com.liferay.object.constants.ObjectPortletKeys;
 import com.liferay.object.definition.tree.Edge;
 import com.liferay.object.definition.tree.Node;
 import com.liferay.object.definition.tree.Tree;
@@ -14,55 +14,125 @@ import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectRelationship;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectRelationshipLocalService;
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.vulcan.util.LocalizedMapUtil;
+import com.liferay.portal.kernel.portlet.PortletConfigFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.service.PortletLocalService;
+import com.liferay.portal.kernel.test.AssertUtils;
+import com.liferay.portal.kernel.test.portlet.MockLiferayResourceRequest;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.TimeZoneUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.portlet.PortletException;
 
 /**
  * @author Feliphe Marinho
  */
 public class TreeTestUtil {
 
+	public static void assertTree(
+			Map<String, String[]> expectedMap, Tree actualTree,
+			ObjectDefinitionLocalService objectDefinitionLocalService)
+		throws PortalException {
+
+		Map<String, String[]> actualMap = new LinkedHashMap<>();
+
+		Iterator<Node> iterator = actualTree.iterator();
+
+		while (iterator.hasNext()) {
+			Node node = iterator.next();
+
+			actualMap.put(
+				_getShortName(node, objectDefinitionLocalService),
+				TransformUtil.transformToArray(
+					node.getChildNodes(),
+					childNode -> _getShortName(
+						childNode, objectDefinitionLocalService),
+					String.class));
+		}
+
+		AssertUtils.assertEquals(expectedMap, actualMap);
+	}
+
+	public static void bind(
+			MVCResourceCommand mvcResourceCommand,
+			List<ObjectRelationship> objectRelationships,
+			PortletLocalService portletLocalService)
+		throws PortletException {
+
+		MockLiferayResourceRequest mockLiferayResourceRequest =
+			new MockLiferayResourceRequest();
+
+		mockLiferayResourceRequest.setAttribute(
+			JavaConstants.JAVAX_PORTLET_CONFIG,
+			PortletConfigFactoryUtil.create(
+				portletLocalService.getPortletById(
+					ObjectPortletKeys.OBJECT_DEFINITIONS),
+				null));
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setLocale(LocaleUtil.getSiteDefault());
+		themeDisplay.setTimeZone(TimeZoneUtil.getDefault());
+
+		mockLiferayResourceRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		mockLiferayResourceRequest.addParameter(
+			"objectRelationshipIds",
+			TransformUtil.transformToArray(
+				objectRelationships,
+				objectRelationship -> String.valueOf(
+					objectRelationship.getObjectRelationshipId()),
+				String.class));
+
+		mvcResourceCommand.serveResource(mockLiferayResourceRequest, null);
+	}
+
 	public static Tree createTree(
+			MVCResourceCommand bindObjectDefinitionsMVCResourceCommand,
 			ObjectDefinitionLocalService objectDefinitionLocalService,
 			ObjectRelationshipLocalService objectRelationshipLocalService,
-			TreeFactory treeFactory)
-		throws PortalException {
+			PortletLocalService portletLocalService, TreeFactory treeFactory)
+		throws PortalException, PortletException {
 
 		ObjectDefinition objectDefinitionA =
 			ObjectDefinitionTestUtil.addObjectDefinition(
 				"A", objectDefinitionLocalService);
 
-		objectDefinitionLocalService.updateRootObjectDefinitionId(
-			objectDefinitionA.getObjectDefinitionId(),
-			objectDefinitionA.getObjectDefinitionId());
-
 		ObjectDefinition objectDefinitionAA =
 			ObjectDefinitionTestUtil.addObjectDefinition(
 				"AA", objectDefinitionLocalService);
 
-		_bind(
-			objectDefinitionAA, objectDefinitionLocalService,
-			objectRelationshipLocalService, objectDefinitionA,
-			objectDefinitionA);
-		_bind(
-			ObjectDefinitionTestUtil.addObjectDefinition(
-				"AAA", objectDefinitionLocalService),
-			objectDefinitionLocalService, objectRelationshipLocalService,
-			objectDefinitionAA, objectDefinitionA);
-		_bind(
-			ObjectDefinitionTestUtil.addObjectDefinition(
-				"AAB", objectDefinitionLocalService),
-			objectDefinitionLocalService, objectRelationshipLocalService,
-			objectDefinitionAA, objectDefinitionA);
-
-		_bind(
-			ObjectDefinitionTestUtil.addObjectDefinition(
-				"AB", objectDefinitionLocalService),
-			objectDefinitionLocalService, objectRelationshipLocalService,
-			objectDefinitionA, objectDefinitionA);
+		bind(
+			bindObjectDefinitionsMVCResourceCommand,
+			Arrays.asList(
+				ObjectRelationshipTestUtil.addObjectRelationship(
+					objectRelationshipLocalService, objectDefinitionA,
+					objectDefinitionAA),
+				ObjectRelationshipTestUtil.addObjectRelationship(
+					objectRelationshipLocalService, objectDefinitionAA,
+					ObjectDefinitionTestUtil.addObjectDefinition(
+						"AAA", objectDefinitionLocalService)),
+				ObjectRelationshipTestUtil.addObjectRelationship(
+					objectRelationshipLocalService, objectDefinitionAA,
+					ObjectDefinitionTestUtil.addObjectDefinition(
+						"AAB", objectDefinitionLocalService)),
+				ObjectRelationshipTestUtil.addObjectRelationship(
+					objectRelationshipLocalService, objectDefinitionA,
+					ObjectDefinitionTestUtil.addObjectDefinition(
+						"AB", objectDefinitionLocalService))),
+			portletLocalService);
 
 		return treeFactory.create(objectDefinitionA.getObjectDefinitionId());
 	}
@@ -81,32 +151,16 @@ public class TreeTestUtil {
 			edge.getObjectRelationshipId());
 	}
 
-	private static ObjectDefinition _bind(
-			ObjectDefinition objectDefinition,
-			ObjectDefinitionLocalService objectDefinitionLocalService,
-			ObjectRelationshipLocalService objectRelationshipLocalService,
-			ObjectDefinition parentObjectDefinition,
-			ObjectDefinition rootObjectDefinition)
+	private static String _getShortName(
+			Node node,
+			ObjectDefinitionLocalService objectDefinitionLocalService)
 		throws PortalException {
 
-		ObjectRelationship objectRelationship =
-			objectRelationshipLocalService.addObjectRelationship(
-				TestPropsValues.getUserId(),
-				parentObjectDefinition.getObjectDefinitionId(),
-				objectDefinition.getObjectDefinitionId(), 0,
-				ObjectRelationshipConstants.DELETION_TYPE_CASCADE,
-				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
-				StringUtil.randomId(),
-				ObjectRelationshipConstants.TYPE_ONE_TO_MANY);
+		ObjectDefinition objectDefinition =
+			objectDefinitionLocalService.getObjectDefinition(
+				node.getObjectDefinitionId());
 
-		objectRelationshipLocalService.updateObjectRelationship(
-			objectRelationship.getObjectRelationshipId(), 0,
-			ObjectRelationshipConstants.DELETION_TYPE_CASCADE, true,
-			objectRelationship.getLabelMap());
-
-		return objectDefinitionLocalService.updateRootObjectDefinitionId(
-			objectDefinition.getObjectDefinitionId(),
-			rootObjectDefinition.getObjectDefinitionId());
+		return objectDefinition.getShortName();
 	}
 
 }
