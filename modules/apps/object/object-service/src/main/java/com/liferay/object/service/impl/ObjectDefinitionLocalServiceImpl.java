793ff4f4ad802e4a85f78dea9dd4dafc86550484
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2023-09-11 14:27:32 -0300

    LPS-193257 Redeploy the ObjectDefinition if it already is published, when performing bind or unbind. Apply isRootDescendantNode in missing needed place

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectDefinitionLocalServiceImpl.java
@@ -1161,9 +1161,7 @@ public class ObjectDefinitionLocalServiceImpl
 			objectDefinitionPersistence.findByPrimaryKey(
 				rootObjectDefinitionId);
 
-		if ((objectDefinitionId != rootObjectDefinitionId) &&
-			(rootObjectDefinition.getRootObjectDefinitionId() == 0)) {
-
+		if (rootObjectDefinition.isRootDescendantNode()) {
 			throw new ObjectDefinitionRootObjectDefinitionIdException(
 				"Object definition " + rootObjectDefinitionId +
 					" is not a root object definition");
@@ -1171,7 +1169,15 @@ public class ObjectDefinitionLocalServiceImpl
 
 		objectDefinition.setRootObjectDefinitionId(rootObjectDefinitionId);
 
-		return objectDefinitionPersistence.update(objectDefinition);
+		objectDefinition = objectDefinitionPersistence.update(objectDefinition);
+
+		if (objectDefinition.isRootDescendantNode() &&
+			objectDefinition.isApproved()) {
+
+			deployObjectDefinition(objectDefinition);
+		}
+
+		return objectDefinition;
 	}
 
 	@Indexable(type = IndexableType.REINDEX)
